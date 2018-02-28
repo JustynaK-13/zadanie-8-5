629ccac4a5f942d38eb46b6b7af0760f0a0103b4
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-02-28 20:43:16 +0100

    Delete prompt function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 var a = 4,
 	h = 3,
 	triangleArea = (a*h/2);
-var triangleArea = prompt('enter 6');
 alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
